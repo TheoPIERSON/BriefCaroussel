91e7e4786458c9c8a7632e17348e6433221acb1c
Theo Pierson <thheopierson@gmail.com>
2023-06-12 12:08:32 +0200

    build: change html and js file for a more proper code

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 "use strict";
 
-let slide = ["carousel1.jpg", "carousel2.jpg", "carousel4.jpg", "windows.jpg", "t725.png", "Phase_4.jpg"];
+const slide = ["carousel1.jpg", "carousel2.jpg", "carousel4.jpg", "windows.jpg", "t725.png", "Phase_4.jpg"];
 let index = 0;
 
-function ChangeSlide(sens) {
-  index = index + sens;
+let buttonPrev = document.getElementById("buttonPrev");
+var buttonNext = document.getElementById("buttonNext");
+
+function ChangeSlide(increment) {
+  index = index + increment;
   if (index < 0) {
     index = slide.length - 1;
   }
   if (index > slide.length - 1) index = 0;
   document.getElementById("slide").src = slide[index];
 }
-function prevImage() {
-  // on button click change to previous image
-}
+
+buttonPrev.addEventListener("click", function () {
+  ChangeSlide(-1);
+});
+
+buttonNext.addEventListener("click", function () {
+  ChangeSlide(1);
+});
