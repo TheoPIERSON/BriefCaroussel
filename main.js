63e09c7882375cd542365522bf229e33cb87d78e
Theo Pierson <thheopierson@gmail.com>
2023-06-12 10:06:24 +0200

    build: add CSS and JS file and start writting down what function do

---
+++ main.js
@@ -0,0 +1,10 @@
+const divImage = document.getElementById("carousselImages");
+const buttonNext = document.getElementsById("buttonNext");
+const buttonPrev = document.getElementById("buttonPrev");
+
+function changeImage() {
+  //on button click change to next image
+}
+function prevImage() {
+  // on button click change to previous image
+}
