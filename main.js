3ae04e902191daf65962d3fb99604e735e3bd23b
Theo Pierson <thheopierson@gmail.com>
2023-06-12 11:15:02 +0200

    buil: functionnal carousel

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const divImage = document.getElementById("carousselImages");
-const buttonNext = document.getElementsById("buttonNext");
+"use strict";
+
+const buttonNext = document.getElementById("buttonNext");
 const buttonPrev = document.getElementById("buttonPrev");
 
-let arrayOfImages = [];
-arrayOfImages.push();
-function changeImage() {
-  //on button click change to next image
+let slide = ["carousel1.jpg", "carousel2.jpg", "carousel4.jpg"];
+let index = 0;
+
+function ChangeSlide(sens) {
+  index = index + sens;
+  if (index < 0) {
+    index = slide.length - 1;
+  }
+  if (index > slide.length - 1) index = 0;
+  document.getElementById("slide").src = slide[index];
 }
 function prevImage() {
   // on button click change to previous image
