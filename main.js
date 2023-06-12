e6cb4d4c805af61e7ff7d65bbe64f81bc9a1a7d7
Theo Pierson <thheopierson@gmail.com>
2023-06-12 10:14:54 +0200

    docs: change images name

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const divImage = document.getElementById("carousselImages");
 const buttonNext = document.getElementsById("buttonNext");
 const buttonPrev = document.getElementById("buttonPrev");
 
+let arrayOfImages = [];
+arrayOfImages.push();
 function changeImage() {
   //on button click change to next image
 }
