99506882945721e0c241447744d17c43c99bd054
Theo Pierson <thheopierson@gmail.com>
2023-06-13 09:29:38 +0200

    docs: change JS comments

--- main.js
+++ main.js
@@ -11,18 +11,11 @@ let index = 0;
 
 // On déclare la fonction ChangeSlide qui prend le paramètre increment et qui va indiquer dans quelle direction le tableau doit changer.
 function ChangeSlide(increment) {
-  // On met à jour la variable index en ajoutant la valeur d'increment.
-  // Cela permet de changer l'index de l'image affichée dans le diaporama.
   index = index + increment;
-
-  // Si la valeur de index est inférieur à zéro alors on met à jour index afin de passer à la dernière image du tableau.
   if (index < 0) {
     index = slide.length - 1;
   }
-
-  // On vérifie si index est supérieur à la longueur du tableau, si oui alors on revient à l'index 0.
   if (index > slide.length - 1) index = 0;
-
   //On met à jour l'image correspondante à l'index auquel nous sommes.
   document.getElementById("slide").src = slide[index];
 }
